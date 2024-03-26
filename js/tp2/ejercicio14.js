7650a2176f4b3891e83a843f24cae0dc54db41d9
Leandro Vinticol <leovinticol@gmail.com>
2024-03-26 16:32:18 -0300

    agregamos validacion al ejercicio 14 y realizamos ejercicio 15

--- js/tp2/ejercicio14.js
+++ js/tp2/ejercicio14.js
@@ -1,15 +1,23 @@
 //14. Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 
-const cadena = 'hola que tal';
-let resultado = '';
+let cadena;
+let resultado;
 
-for(let i = 0; i < cadena.length; i++ ){
-    const caracter = cadena.at(i);
-    if(i === cadena.length - 1){
-        resultado += caracter;
-    }else{
-        resultado += caracter + '-';
-    }
+do {
+  cadena = prompt("Ingrese un texto");
+  if (cadena === null || cadena.trim() === "") {
+    alert("Ingrese un texto válido");
+  } else {
+    break;
+  }
+} while (true);
+
+for (let i = 0; i < cadena.length; i++) {
+  const caracter = cadena.at(i);
+  if (i === cadena.length - 1) {
+    resultado += caracter;
+  } else {
+    resultado += caracter + "-";
+  }
 }
-
 console.log(resultado);
